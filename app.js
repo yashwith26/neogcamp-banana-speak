32143e65cc24f9788c79d685106923bbae030f9c
yashwith26 <srysmk483@gmail.com>
2023-01-25 16:41:22 +0530

    complete

--- app.js
+++ app.js
@@ -2,10 +2,32 @@ var btntranslate =document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
 
-console.log(txtInput);
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL +"?" +"text=" + text
+    
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something wrong with server! try again after some time")
+}
 
 function clickHandler(){
-    outputDiv.innerText="jhhkhkjhhhk " + txtInput.value;
-}
+    
+    var inputText=txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText= json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
+        
+    .catch(errorHandler)
+};
 btntranslate.addEventListener("click", clickHandler)
 
